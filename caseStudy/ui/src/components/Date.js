8eed815ccc80388fe4001b0581e31f2523a9a7d8
evahello7 <47020637+evahello7@users.noreply.github.com>
2019-05-15 16:10:51 -0500

    Update Data.js

--- caseStudy/ui/src/components/Date.js
+++ caseStudy/ui/src/components/Date.js
@@ -26,7 +26,73 @@
  * https://hacker0x01.github.io/react-datepicker/
  */
 
-import React from 'react';
+
+ import React, { Component } from 'react';
+ import DatePicker from 'react-datepicker';
+ import moment from 'moment';
+
+ //import 'react-datepicker/dist/react-datepicker.css';
+ //import 'bootstrap/dist/css/bootstrap.min.css';
+
+ class Date extends Component {
+
+   constructor (props) {
+     super(props)
+     this.state = {
+       startDate: moment(),
+       endDate: moment()
+     };
+     this.handleChange = this.handleChange.bind(this);
+     this.handleSubmit = this.handleSubmit.bind(this);
+   }
+
+   handleChange(date_start, date_end) {
+     this.setState({
+       startDate: date_start,
+       endDate: date_end
+     })
+   }
+
+   handleSubmit(e) {
+       e.preventDefault();
+       let main = this.state.startDate + this.state.endDate
+       console.log(main.format('L'));
+     }
+
+     render() {
+       return (
+         <div className="date">
+           <form onSubmit={ this.handleSubmit }>
+             <div className="form-group">
+               <label>Start Date: </label>
+               <DatePicker
+                 selected={ this.state.startDate }
+                 onChange={ this.handleChange }
+                 name="startDate"
+                 dateFormat="MM/DD/YYYY"
+               />
+               <label>End Date: </label>
+                <DatePicker
+                  selected={ this.state.endDate }
+                  onChange={ this.handleChange }
+                  name="endDate"
+                    dateFormat="MM/DD/YYYY"
+                 />
+             </div>
+             <div className="form-group">
+               <button className="btn btn-success">Add Date</button>
+               <p><strong>{this.props.text}</strong></p>
+               <div className="date-input">
+               </div>
+             </div>
+           </form>
+         </div>
+
+       );
+     }
+   }
+
+/*import React from 'react';
 //import DatePicker from 'react-datepicker'; UNCOMMENT this line if you are using the DatePicker component
 import moment from 'moment';
 
@@ -46,18 +112,20 @@ class Date extends React.Component {
     }
 
     handleChange(date) {
+        this.props.onChange(date);
         /**
          * TODO
          * Set the state. Call this.props.onChange with the date argument
          * to propagate the change to App component, which will handle it via its
          * own onChange prop.
          */
-    }
+/*   }
 
     render() {
         return (
             <div className="date">
                 {
+                  <input type='text' date={this.props.date} onChange={this.handleChange.bind(this)} />
                     /**
                      * TODO
                      * Render the date picker component with a date format of "MM/DD/YYYY".
@@ -65,7 +133,7 @@ class Date extends React.Component {
                      * This method should set the state to the date argument passed in the parameter.
                      *
                      */
-                }
+/*                }
                 <p><strong>{this.props.text}</strong></p>
                 <div className="date-input">
 
@@ -77,4 +145,7 @@ class Date extends React.Component {
 
 }
 
+*/
+
+export default Date
 // Don't forget to export your component!
