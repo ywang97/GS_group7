7dde6faa0c6727b3f53e88bfd4753b9f17162aab
amandaliusa <39231390+amandaliusa@users.noreply.github.com>
2019-05-16 08:34:40 -0400

    Update App.js

--- caseStudy/ui/src/App.js
+++ caseStudy/ui/src/App.js
@@ -25,12 +25,6 @@ import './style/App.css';
  import Date from './components/Date';
  import Charts from './components/Charts';
  import LineChart from './components/charts/LineChart';
- var dataone;
- dataone = [245.97, 237.4, 237.4, 220.55, 220.55, 205.43, 192, 235.07, 207.68, 207.68, 207.68, 226.71, 244.82,
-    225.34, 225.34, 218.15, 236.29, 253.35, 242.83, 254.15, 225.34, 235.84, 224.29, 210.08, 210.08, 199.43, 199.43,
-    213.77, 187.99, 203.95, 187.26, 199.5, 218.32, 218.32, 235.45, 235.45, 196.94, 222.98, 242.38, 216.7, 235.57, 235.57,
-    222.94, 235.08, 215.31, 233.41, 213.72, 213.72, 230.46, 219.9, 237.9, 226.41, 243.61, 253.94, 264.28, 251.53, 264.61,
-    248.97, 242.38, 231.23, 247.33, 235.08, 235.08, 238.49, 250.39, 232.79, 243.86, 262.78];
 
 class App extends React.Component{
     constructor(props) {
@@ -91,7 +85,6 @@ class App extends React.Component{
                         <Date handleChange={this.startdate, this.enddate} />
                       <section>
                       <div className = "chart">
-                      <LineChart dataOne={dataone}/>
                       </div>
                       </section>
                    </div>
